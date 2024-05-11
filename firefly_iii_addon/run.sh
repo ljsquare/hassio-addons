e987554bd28a6a56a1fa78d444fbac29292223ef
ljsquare <laurens-jan@merkx-ewals.nl>
2024-05-11 18:01:22 +0200

    updates

--- firefly_iii_addon/run.sh
+++ firefly_iii_addon/run.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/bashio
 set +u
 
-export DB_HOST="Test"
-echo $DB_HOST
+export DB_HOST="$(bashio::config 'DB_HOST')")
+export APP_KEY="$(bashio::config 'APP_KEY')")
+export DB_CONNECTION="$(bashio::config 'DB_CONNECTION')")
+export DB_PORT="$(bashio::config 'DB_PORT')")
+export DB_DATABASE="$(bashio::config 'DB_DATABASE')")
+export DB_USERNAME="$(bashio::config 'DB_USERNAME')")
+export DB_PASSWORD="$(bashio::config 'DB_PASSWORD')")
+
 
 source /usr/local/bin/entrypoint.sh
