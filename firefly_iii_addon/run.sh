2559657832f8ad1fa776de6718b386143753ba3c
ljsquare <laurens-jan@merkx-ewals.nl>
2024-05-11 19:23:33 +0200

    Update run.sh

--- firefly_iii_addon/run.sh
+++ firefly_iii_addon/run.sh
@@ -8,21 +8,8 @@ export DB_PORT=$(bashio::services "mysql" "port")
 export DB_USERNAME=$(bashio::services "mysql" "username")
 export DB_PASSWORD=$(bashio::services "mysql" "password")
 
-export DB_DATABASE=$(\
-    mysql \
-        -u "${DB_USERNAME}" -p"${DB_PASSWORD}" \
-        -h "${DB_HOST}" -P "${DB_PORT}" \
-        --skip-column-names \
-        -e "SHOW DATABASES LIKE 'firefly';"
-)
-
-if ! bashio::var.has_value "${DB_DATABASE}"; then
-    bashio::log.info "Creating database for FireFly"
-    mysql \
-        -u "${DB_USERNAME}" -p"${DB_PASSWORD}" \
-        -h "${DB_HOST}" -P "${DB_PORT}" \
-            --execute="CREATE DATABASE IF NOT EXISTS firefly"
-fi
+export DB_DATABASE="firefly"
+mysql -u "${DB_USERNAME}" -p"${DB_PASSWORD}" -h "${DB_HOST}" -P "${DB_PORT}" --execute="CREATE DATABASE IF NOT EXISTS firefly"
 
 
 source /usr/local/bin/entrypoint.sh
