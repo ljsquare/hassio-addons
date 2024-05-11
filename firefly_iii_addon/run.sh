d31759e6550fc308f1dcd698df806d1037422e4e
ljsquare <laurens-jan@merkx-ewals.nl>
2024-05-11 19:42:59 +0200

    Update run.sh

--- firefly_iii_addon/run.sh
+++ firefly_iii_addon/run.sh
@@ -7,7 +7,7 @@ export DB_CONNECTION="mysql"
 export DB_PORT=$(bashio::services "mysql" "port")
 export DB_USERNAME=$(bashio::services "mysql" "username")
 export DB_PASSWORD=$(bashio::services "mysql" "password")
-export $DKR_RUN_PASSPORT_INSTALL="false"
+export DKR_RUN_PASSPORT_INSTALL=false
 
 export DB_DATABASE="firefly"
 mysql -u "${DB_USERNAME}" -p"${DB_PASSWORD}" -h "${DB_HOST}" -P "${DB_PORT}" --execute="CREATE DATABASE IF NOT EXISTS firefly"
